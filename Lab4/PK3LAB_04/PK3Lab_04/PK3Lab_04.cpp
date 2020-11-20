47b59fec9e4b8e871a1c6bcb4d87344d88999220
DawidFurs <43808802+DawidFurs@users.noreply.github.com>
2020-11-20 08:34:36 +0100

    Update PK3Lab_04.cpp

--- Lab4/PK3LAB_04/PK3Lab_04/PK3Lab_04.cpp
+++ Lab4/PK3LAB_04/PK3Lab_04/PK3Lab_04.cpp
@@ -2,54 +2,28 @@
 #include<iostream>
 #include<string>
 using namespace std;
-int main(int argc, char* argv[])
+int main()
 {
-    Szachownica* stol = new Szachownica;
-    //stol->clear();
-  // stol->display();
 
-    Wieza w1;
-    Goniec g1;
-
-    //Figura* wsk = &w1;
-    Figura* wsk = &g1;
-    wsk->set(stol, 0, 0);
-
-
-
-    stol->display();
-    wsk->range();
-    wsk->remove();
-    stol->clear();
-    wsk->set(stol, 2, 3);
-
-    cout << endl;   cout << endl;
-    stol->display();
-    cout << endl;
-    wsk->range();
-    cout << endl;
-    cout << endl;
-    system("CLS");
-    //2
     Szachownica* stol1 = new Szachownica, * stol2 = new Szachownica;//tworzenei dwoch szachownic
-    Goniec g2;
-    g2.set(stol1, 3, 3);
+    Goniec g;
+    g.set(stol1, 3, 3);
     stol1->display();
     cout << endl << endl;
     //goniec wieza
-    Wieza w2;
-    Figura* wsk2;
-    wsk2 = &g2;
-    wsk2->set(stol2, 3, 3);
+    Wieza w;
+    Figura* wsk;
+    wsk = &g;
+    wsk->set(stol2, 3, 3);
 
-    wsk2 = &w2;
-    wsk2->set(stol2, 3, 2);
+    wsk = &w;
+    wsk->set(stol2, 3, 2);
     stol2->display();
     //3
     cout << "zasieg wiezy" << endl;
-    w2.range();
+    w.range();
     cout << "zasieg gonca" << endl;
-    g2.range();
+    g.range();
     stol2->clear();
 
     return 0;
